55d80087d49aba253ebdb9a28a4f9609478d3714
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-16 18:59:25 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,22 @@
+print(10, end="")
+
+print("Talish")
+
+print(10+9)
+
+print("My name is Talish",0, sep="_")
+
+print('''The tale of two cities
+      
+            NKWQJNCKJCN
+        DWKNLN''')
+
+a=10
+print("value", a)
+
+b=25
+print("value", b)
+
+name=input("Enter your name")
+print("welcome", name)
+
